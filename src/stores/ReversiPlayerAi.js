9115fbebfd5696aff2d4f42e89a48105e9008f4e
KoharaKazuya <reflect.results@gmail.com>
2016-01-02 23:19:15 +0900

    WebWorker のファイルパスを呼び出し元ファイルを起点とした相対パスに

--- src/stores/ReversiPlayerAi.js
+++ src/stores/ReversiPlayerAi.js
@@ -1,4 +1,16 @@
-const aiWorker = new Worker('ReversiPlayerAiWorker.js');
+// WebWorker のファイルパスのこのファイルの相対パスから取得できるように、
+// このファイルのパスを取得する
+const jsPath = (() => {
+  const scripts = document.querySelectorAll('script');
+  for (let i = 0; i < scripts.length; i++) {
+    const matches = scripts[i].src.match(/^(.*\/)bundle\.js$/);
+    if (matches) {
+      return matches[1];
+    }
+  }
+})();
+
+const aiWorker = new Worker(jsPath + 'ReversiPlayerAiWorker.js');
 
 export default {
   getNextActionByAi(cells, token, depth) {
